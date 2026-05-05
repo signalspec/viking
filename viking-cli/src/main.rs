2d4b46dec63f8289bf24acbb8ed033110dd1dbde
Kevin Mehall <km@kevinmehall.net>
2026-05-04 19:44:48 -0600

    CLI improvements

--- viking-cli/src/main.rs
+++ viking-cli/src/main.rs
@@ -1,6 +1,8 @@
 use std::io::{self, Write};
+use owo_colors::OwoColorize;
 
 use futures_lite::future::block_on;
+use viking_protocol::protocol;
 
 fn main() {
     env_logger::init();
@@ -13,14 +15,26 @@ fn info(intf: &viking_io::Interface) -> Result<(), io::Error> {
     let mut w = io::stdout().lock();
     let desc = intf.descriptor();
 
-    writeln!(w, "Version {}", desc.version())?;
-    writeln!(w, "  Commands up to {} bytes", desc.max_cmd_len())?;
-    writeln!(w, "  Response up to {} bytes", desc.max_res_len())?;
+    writeln!(w, "Viking protocol version {}", desc.version())?;
+    writeln!(w, "Commands up to {} bytes", desc.max_cmd_len())?;
+    writeln!(w, "Response up to {} bytes", desc.max_res_len())?;
+    writeln!(w)?;
 
     for (_, resource) in desc.resources() {
-        writeln!(w, "{}", resource.name())?;
+        writeln!(w, "{}", resource.name().bold().blue())?;
         for (_, mode) in resource.modes() {
-            writeln!(w, "  {} {:04X}", mode.name().unwrap_or(""), mode.protocol())?;
+            let protocol_id = mode.protocol();
+            if let Some(protocol_name) = protocol::protocol_name(protocol_id) {
+                write!(w, "    {protocol_name}")?;
+            } else {
+                write!(w, "    unknown({protocol_id:04X})")?;
+            };
+
+            if let Some(mode_name) = mode.name() {
+                write!(w, " \"{mode_name}\"")?;
+            }
+
+            writeln!(w)?;
         }
     }
     Ok(())
--- viking-protocol/src/protocol/mod.rs
+++ viking-protocol/src/protocol/mod.rs
@@ -9,3 +9,19 @@ pub mod spi;
 pub mod cmd {
     pub const DELAY: u8 = 0;
 }
+
+pub fn protocol_name(protocol: u16) -> Option<&'static str> {
+    Some(match protocol {
+        gpio::pin::PROTOCOL => "gpio_pin",
+        gpio::level_interrupt::PROTOCOL => "gpio_level_interrupt",
+        led::binary::PROTOCOL => "led",
+        i2c::controller::PROTOCOL => "i2c_controller",
+        i2c::scl::PROTOCOL => "i2c_sda_pin",
+        i2c::sda::PROTOCOL => "i2c_scl_pin",
+        spi::controller::PROTOCOL => "spi_controller",
+        spi::sck_pin::PROTOCOL => "spi_sck_pin",
+        spi::sdi_pin::PROTOCOL => "spi_sdi_pin",
+        spi::sdo_pin::PROTOCOL => "spi_sdo_pin",
+        _ => return None
+    })
+}
